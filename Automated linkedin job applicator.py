03b3d71c75ccd30d23bb802f9a7703e63ffc1960
SAISHANMUKHASANGU <137365406+SAISHANMUKHASANGU@users.noreply.github.com>
2024-09-22 21:16:21 +0530

    Add files via upload

---
+++ Automated linkedin job applicator.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+chrome_options= webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach",True)
+driver =webdriver.Chrome(options=chrome_options)
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3931156184&distance=25&f_AL=true&geoId=102713980&keywords=python&origin=JOBS_HOME_KEYWORD_HISTORY&refresh=true")
+
+
+signin_button=driver.find_element(By.XPATH,value='/html/body/div[1]/header/nav/div/a[2]')
+signin_button.click()
+
+username=driver.find_element(By.ID,value="username")
+username.send_keys("sangusaishanmukha@gmail.com")
+
+password=driver.find_element(By.ID,value="password")
+password.send_keys("shanu@2003")
+
+signin_button_final=driver.find_element(By.XPATH,value='//*[@id="organic-div"]/form/div[3]/button')
+signin_button_final.click()
+
+apply_button=driver.find_element(By.ID,value='ember56')
+apply_button.click()
+
+driver.quit()
